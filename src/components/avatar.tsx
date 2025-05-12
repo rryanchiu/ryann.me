15e93e78375855ecb76f6c191bf0b51a7664754e
Ryan <xiaoliang.q@outlook.com>
2025-05-12 16:43:49 +0800

    update: avatar

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,12 +1,12 @@
 import md5 from 'blueimp-md5';
 
 const Avatar = ({email, size = 80}: { email: string, size?: number }) => {
-    const hash = md5(email.trim().toLowerCase());
-    const url = `https://www.gravatar.com/avatar/${hash}?s=${size}&d=identicon`;
+    // const hash = md5(email.trim().toLowerCase());
+    // const url = `https://www.gravatar.com/avatar/${hash}?s=${size}&d=identicon`;
 
     return (
         <img
-            src={url}
+            src='avatar.jpeg'
             alt="User Avatar"
             width={size}
             height={size}
